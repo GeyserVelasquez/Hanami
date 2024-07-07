371f143f6f3f217c4f955c83fbdeff280b2077aa
Geyser Velasquez <gvelasquez.9312@unimar.edu.ve>
2024-07-06 23:17:58 -0400

    Implementacion de One Page

--- www/js/onePage/movieDetails.js
+++ www/js/onePage/movieDetails.js
@@ -1,6 +1,6 @@
 $(document).ready(function () {
-    const toggleBottomCanvas = document.getElementById('btn-review');
-    const offcanvasBottom = new bootstrap.Offcanvas(document.getElementById('review'));
+    const toggleBottomCanvas = document.getElementById('btn-action');
+    const offcanvasBottom = new bootstrap.Offcanvas(document.getElementById('actionDetails'));
 
     toggleBottomCanvas.addEventListener('click', function (event) {
         event.stopPropagation(); // Evitar que el clic cierre el offcanvas padre
